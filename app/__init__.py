13f223ee9b02d54cde7441497b4b7ce4da36552a
sadsaviour <30339972+sadsaviour@users.noreply.github.com>
2017-12-08 10:27:35 +0300

    auth dev
    add secret

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
+lm = LoginManager(app)
 db = SQLAlchemy(app)
 
 from app import views, models
--- config.py
+++ config.py
@@ -1,14 +1,25 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+OAUTH_CREDENTIALS = {
+    'facebook': {
+        'id': '1144097435721170',
+        'secret': '5510e1b3b7c74d5e0a5e70715c7ac45f'
+    },
+#    'twitter': {
+#        'id': '3RzWQclolxWZIMq5LJqzRZPTl',
+#        'secret': 'm9TEd58DSEtRrZHpz2EjrV9AhsBRxKMo8m3kuIZj3zLwzwIimt'
+#    }
+
+}
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
